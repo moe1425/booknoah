03c99154c7ef8a93337b67a6d6e7924a8669e4e8
EC2 Default User <ec2-user@ip-172-31-43-97.ap-northeast-1.compute.internal>
2022-09-04 06:26:38 +0000

    userとadminの追加

---
+++ app/controllers/user/sessions_controller.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+class User::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+  before_action :user_state, only: [:create]
+  
+  def after_sign_in_path_for(resource)
+    my_page_path(resource)
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+  
+  protected
+  # 退会しているかを判断するメソッド
+  def user_state
+    @user = User.find_by(email: params[:user][:email])
+    return if !@user
+    
+    if @user.valid_password?(params[:user][:password]) && @user.is_deleted == true
+      redirect_to new_user_registration_path
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
+  
+  # 顧客用
+  # URL /users/sign_in ...
+  devise_for :users, controllers: {
+    registrations: "user/registrations",
+    sessions: 'user/sessions'
+  }
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  scope module: :user do
+    root to: 'homes#top'
+    get '/about' => 'homes#about'
+    
+  end
+  
+  namespace :admin do
+  end
 end
