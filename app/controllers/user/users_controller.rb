99051af44fa4432e9d324a26ed03d1303902e8cc
EC2 Default User <ec2-user@ip-172-31-43-97.ap-northeast-1.compute.internal>
2022-09-05 02:15:44 +0000

    マイページとプロフィール編集画

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -6,14 +6,26 @@ class User::UsersController < ApplicationController
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to my_page_path(@user.id)
   end
+  
 
   def unsubscribe
   end
 
   def withdraw
   end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction, :is_deleted)
+  end
+  
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import'jquery'
 import "script.js"
+import "bootstrap";
+import "../stylesheets/application.scss";
 
 Rails.start()
 Turbolinks.start()
