2d550d2c6b3db8606c33f981c4bfeea78910bf7f
EC2 Default User <ec2-user@ip-172-31-43-97.ap-northeast-1.compute.internal>
2022-09-11 08:45:40 +0000

    レビューのnewページ作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+
 //= require jquery.raty.js
 
 import Rails from "@rails/ujs"
@@ -12,7 +13,11 @@ import'jquery'
 import "script.js"
 import "bootstrap";
 import "../stylesheets/application.scss";
+// import raty from 'raty-js'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+window.$ = window.jQuery = require('jquery');
+require('packs/raty')
