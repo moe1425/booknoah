d282483ab7ac54b25fb2964e13c2835dcd11f113
EC2 Default User <ec2-user@ip-172-31-43-97.ap-northeast-1.compute.internal>
2022-09-12 07:19:48 +0000

    バリデーション実装

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,8 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :book
   
+  validates :title, length: { minimum: 2, maximum: 50 }, presence: true
+  validates :content, length: { minimum: 2, maximum: 250 }, presence: true
   validates :star, numericality: {
     less_than_or_equal_to: 5,
     greater_than_or_equal_to: 1
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,21 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-         
-  has_many :reviews, dependent: :destroy       
-  has_one_attached :profile_image
-        
+  class User < ApplicationRecord
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    devise :database_authenticatable, :registerable,
+      :recoverable, :rememberable, :validatable
+    
+    has_many :reviews, dependent: :destroy 
+    has_one_attached :profile_image
+    
+    validates :name, presence: true, length: { minimum: 2, maximum: 50 }
+    validates :introduction, length: { maximum: 255 }
+    
     def get_profile_image(width, height)
-      unless profile_image.attached?
+      if !profile_image.attached?
         file_path = Rails.root.join('app/assets/images/default-image.jpg')
         profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
       end
-      profile_image.variant(resize_to_limit: [width, height])
+      profile_image.variant(resize_to_limit: [width, height]).processed
     end
     
     def self.guest
@@ -21,4 +24,4 @@ class User < ApplicationRecord
         user.name = "ゲストユーザー"
       end
     end
-end
+  end
