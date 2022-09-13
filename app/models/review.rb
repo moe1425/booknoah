eaa3f8494f794bb6b1df5efffa0c915b06915be7
EC2 Default User <ec2-user@ip-172-31-43-97.ap-northeast-1.compute.internal>
2022-09-13 06:35:04 +0000

    レビューバリデーション変更

--- app/models/review.rb
+++ app/models/review.rb
@@ -8,7 +8,7 @@ class Review < ApplicationRecord
   end
   
   validates :title, length: { minimum: 2, maximum: 50 }, presence: true
-  validates :content, length: { minimum: 2, maximum: 250 }, presence: true
+  validates :content, length: { minimum: 2, maximum: 1000 }, presence: true
   validates :star, numericality: {
     less_than_or_equal_to: 5,
     greater_than_or_equal_to: 1
