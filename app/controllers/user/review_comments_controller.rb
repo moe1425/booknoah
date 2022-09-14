2d06c485f4c51f8591187657934acf081d2d0c1c
EC2 Default User <ec2-user@ip-172-31-43-97.ap-northeast-1.compute.internal>
2022-09-14 13:33:06 +0000

    コメント機能

--- app/controllers/user/review_comments_controller.rb
+++ app/controllers/user/review_comments_controller.rb
@@ -1,16 +1,18 @@
   class User::ReviewCommentsController < ApplicationController
     
     def create
+      @book = Book.find(params[:book_id])
       @review = Review.find(params[:review_id])
-      @comment = current_user.review_comments.new(review_comment_params)
-      @comment.review_id = review.id
-      @comment.save
+      @review_comment = current_user.review_comments.new(review_comment_params)
+      @review_comment.review_id = @review.id
+      @review_comment.save
     end
     
     def destroy
+      @book = Book.find(params[:book_id])
       @review = Review.find(params[:review_id])
-      @comment = ReviewComment.find(params[:id])
-      @comment.destroy
+      @review_comment = ReviewComment.find(params[:id])
+      @review_comment.destroy
     end
     
     private
