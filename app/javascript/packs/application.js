ee100981ce8d7b729f07f485b32660e722071580
EC2 Default User <ec2-user@ip-172-31-43-97.ap-northeast-1.compute.internal>
2022-09-20 15:09:23 +0000

    ロゴ

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,9 +10,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import'jquery';
-import "script.js";
-import "bootstrap";
+import "jquery"
+import "script.js"
+import "popper.js"
+import "bootstrap"
 import "../stylesheets/application.scss"
 import '@fortawesome/fontawesome-free/js/all'
 
